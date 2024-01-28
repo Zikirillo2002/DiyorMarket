29987dbed11ace3b47ffe1bbefed5608051ae989
Sharafiddin.Mukhtorov <129146459+SharafiddinMukhtorov@users.noreply.github.com>
2024-01-28 16:51:44 +0500

    added

---
+++ DiyorMarket.MVC/Lesson11/Stores/Customers/ICustomerDataStore.cs
@@ -0,0 +1,6 @@
+﻿namespace Lesson11.Stores.Customers
+{
+    public interface ICustomerDataStore
+    {
+    }
+}
---
+++ DiyorMarket.MVC/Lesson11/Stores/Suppliers/ISupplierDataStore.cs
@@ -0,0 +1,6 @@
+﻿namespace Lesson11.Stores.Suppliers
+{
+    public interface ISupplierDataStore
+    {
+    }
+}
