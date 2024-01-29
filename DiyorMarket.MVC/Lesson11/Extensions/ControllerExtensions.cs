6a641764c02243e245eac10fe80b856aafb52545
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-01-29 10:37:28 +0500

    Add SetViewBagProperties method

--- DiyorMarket.MVC/Lesson11/Extensions/ControllerExtensions.cs
+++ DiyorMarket.MVC/Lesson11/Extensions/ControllerExtensions.cs
@@ -1,6 +1,18 @@
-﻿namespace Lesson11.Extensions
+﻿using Lesson11.Response;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lesson11.Controllers
 {
-    public class ControllerExtensions
+    public static class ControllerExtensions
     {
+        public static void SetViewBagProperties<T>(this Controller controller, GetBaseResponse<T> result)
+        {
+            controller.ViewBag.CategoriesCount = result.Data?.Count();
+            controller.ViewBag.CurrentPage = result.PageNumber;
+            controller.ViewBag.PageSize = result.PageSize;
+            controller.ViewBag.HasNext = result.HasNextPage;
+            controller.ViewBag.HasPrevious = result.HasPreviousPage;
+            controller.ViewBag.TotalPages = result.TotalPages;
+        }
     }
 }
