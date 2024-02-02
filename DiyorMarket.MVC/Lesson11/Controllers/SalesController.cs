a1f84d95895c499cc699764ae06c0aa958f1f62f
khojianvar <khojianvarabdumukhtorov@gmail.com>
2024-02-02 14:32:43 +0500

    div

--- DiyorMarket.MVC/Lesson11/Controllers/SalesController.cs
+++ DiyorMarket.MVC/Lesson11/Controllers/SalesController.cs
@@ -14,16 +14,11 @@ namespace Lesson11.Controllers
 
         public IActionResult Index()
         {
-            var result = _saleDataStore.GetSales();
+            var sales = _saleDataStore.GetSales();
 
-            if (result is null)
-            {
-                return NotFound();
-            }
+            ViewBag.Sales = sales.Data;
 
-            this.SetViewBagProperties(result);
-
-            return View(result.Data);
+            return View();
         }
     }
 }
--- DiyorMarket.MVC/Lesson11/Services/ApiClient.cs
+++ DiyorMarket.MVC/Lesson11/Services/ApiClient.cs
@@ -15,7 +15,7 @@
         {
             var request = new HttpRequestMessage(HttpMethod.Get, _client.BaseAddress?.AbsolutePath + "/" + url);
             request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer",
-                "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjQxMjMiLCJuYW1lIjoiQW52YXIiLCJuYmYiOjE3MDY3NzYyNzMsImV4cCI6MTcwNjg2MjY3MywiaXNzIjoiYW52YXItYXBpIiwiYXVkIjoiYW52YXItbW9iaWxlIn0.BBNjyF6CqMSkNarhj7pguBgnV6ZNa4wugxEjkuQynjA");
+                "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjQxMjMiLCJuYW1lIjoiQW52YXIiLCJuYmYiOjE3MDY4NjQwNTUsImV4cCI6MTcwNjk1MDQ1NSwiaXNzIjoiYW52YXItYXBpIiwiYXVkIjoiYW52YXItbW9iaWxlIn0.XHPue1N2XyHGHQi66JVpxqdPH13EGGrCpbHLw7Wrt4Q");
             var response = _client.Send(request);
 
             return response;
